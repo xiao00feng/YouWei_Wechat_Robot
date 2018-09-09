06e7fa56ca717a9ed21590552079bd8f6ee7ce18
shawphychen <1183173382@qq.com>
2018-09-09 14:24:36 +0800

    新建项目

--- README.md
+++
@@ -1,4 +0,0 @@
-# YouWei_Wechat_Robot
-微信机器人
-STD=Science and Technology Department
-WRC=wechat robot core
--- src/main/sql/20171214.sql
+++
@@ -1,2 +0,0 @@
-create 'receiver_address_info',{NAME => 'info'}
-create 'receiver_order_info',{NAME => 'info'}
